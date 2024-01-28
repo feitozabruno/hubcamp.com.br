d17121e6401f8266cd839cef4b390a2e95a840fd
feitozabruno <brunofeitozanascimento@gmail.com>
2024-01-27 21:20:30 -0300

    first commit

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "HubCamp",
+  description: "hubcamp.com.br",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
