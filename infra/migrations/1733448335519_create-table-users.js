3d1d17c8ee3d0360d53759a1899f66ff89957d08
Bruno Feitoza <brunofeitozanascimento@gmail.com>
2024-12-08 15:07:24 -0300

    feat: add function to create user table in migration file

--- infra/migrations/1733448335519_create-table-users.js
+++ infra/migrations/1733448335519_create-table-users.js
@@ -1,18 +1,10 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
-exports.shorthands = undefined;
-
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-exports.up = (pgm) => {};
-
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-exports.down = (pgm) => {};
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: { type: "serial", primaryKey: true },
+    name: { type: "varchar(100)", notNull: true },
+    username: { type: "varchar(50)", notNull: true, unique: true },
+    email: { type: "varchar(100)", notNull: true, unique: true },
+    password: { type: "varchar(255)", notNull: true },
+    created_at: { type: "timestamp", default: pgm.func("current_timestamp") },
+  });
+};
